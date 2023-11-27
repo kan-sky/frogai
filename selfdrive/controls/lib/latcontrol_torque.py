9a7f609763a34f61e614d5dbad51fa26738efd41
SangJo Kim <kandrea@nate.com>
2023-11-27 11:25:20 +0900

    fix error

--- selfdrive/controls/lib/latcontrol_torque.py
+++ selfdrive/controls/lib/latcontrol_torque.py
@@ -142,7 +142,6 @@ class LatControlTorque(LatControl):
     if not active:
       output_torque = 0.0
       pid_log.active = False
-      angle_steers_des = 0.
     else:
       if self.use_steering_angle:
         actual_curvature = -VM.calc_curvature(math.radians(CS.steeringAngleDeg - params.angleOffsetDeg), CS.vEgo, params.roll)
@@ -252,4 +251,4 @@ class LatControlTorque(LatControl):
         pid_log.nnLog = nn_log
 
     # TODO left is positive in this convention
-    return -output_torque, angle_steers_des, pid_log
+    return -output_torque, 0.0, pid_log
