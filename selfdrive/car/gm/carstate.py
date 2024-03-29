c3fad7f8f4d4e32d5df534d6e043fa057da495e0
SangJo Kim <kandrea@nate.com>
2024-03-29 13:12:40 +0900

    fix clusterSpeed

--- selfdrive/car/gm/carstate.py
+++ selfdrive/car/gm/carstate.py
@@ -85,7 +85,7 @@ class CarState(CarStateBase):
       ret.vEgoRaw = mean([ret.wheelSpeeds.fl, ret.wheelSpeeds.fr, ret.wheelSpeeds.rl, ret.wheelSpeeds.rr]) * (105./100.)
       ret.vEgo, ret.aEgo = self.update_speed_kf(ret.vEgoRaw)
     # kans
-    ret.vCluRatio = (ret.vEgoCluster / ret.vEgo) if (ret.vEgo > 3. and ret.vEgoCluster > 3.) else 1.0
+    ret.vCluRatio = (ret.vEgo / ret.vEgoCluster) if (ret.vEgo > 3. and ret.vEgoCluster > 3.) else 1.0
 
     ret.standstill = ret.wheelSpeeds.rl <= STANDSTILL_THRESHOLD and ret.wheelSpeeds.rr <= STANDSTILL_THRESHOLD
 
